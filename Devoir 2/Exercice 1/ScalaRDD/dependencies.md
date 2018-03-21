58f08db84f1d87689d00bbaf466967298439f03c
Sidney Congard <sidney.congard@gmail.com>
2018-03-20 21:57:22 -0400

    finished Scala application, which write output in ScalaRDD/spells

--- Devoir 2/Exercice 1/ScalaRDD/dependencies.md
+++ Devoir 2/Exercice 1/ScalaRDD/dependencies.md
@@ -1,4 +1,5 @@
 
- - JDK 9.0.4
+ - JDK 8.0.162
  - Scala 2.11.12
  - Spark 2.3.0
+ - C:/hadoop/bin/winutils.exe (from haddop 2.7.1)
--- Devoir 2/Exercice 1/ScalaRDD/src/Main.scala
+++ Devoir 2/Exercice 1/ScalaRDD/src/Main.scala
@@ -1,32 +1,75 @@
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import java.io.File
+
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.io.Source
+import scala.concurrent.duration._
 import scala.util.parsing.json.JSON
 
-class MonsterModel(name: String, spells: List[String]) {
-    override def toString : String = "{ name: " + name + ", spells: " + spells + " }"
-}
-
-class CastExtractor[T] {
-    def unapply(a:Any): Option[T] = Some(a.asInstanceOf[T])
-}
 
 object Main extends App {
-    val jFile = scala.io.Source.fromFile("../monsters.json")
-    val jString = try jFile.mkString finally jFile.close()
-    val Some(json) = JSON.parseFull(jString)
 
-    val exList    = new CastExtractor[List[Any]]
-    val exMap     = new CastExtractor[Map[String, Any]]
-    val exStr     = new CastExtractor[String]
-    val exStrList = new CastExtractor[List[String]]
+    implicit val executor: ExecutionContextExecutor = ExecutionContext.global
 
-    val monsters = for {
-        // 'json' is wrapped into a list to allow for expression
-        exList(monsters) <- List(json)
-        exMap(monster)   <- monsters
-        exStr(name)       = monster("name")
-        exStrList(spells) = monster("spells")
-    } yield {
-        new MonsterModel(name, spells)
+    val fSparkContext = Future {
+        println("Initialize Spark ...")
+
+        System.setProperty("hadoop.home.dir", "C:/hadoop")
+        val conf = new SparkConf().setAppName("ScalaRDD").setMaster("local[*]")
+        Logger.getRootLogger.setLevel(Level.WARN)
+        new SparkContext(conf)
     }
-    monsters.foreach(println(_))
+
+    val fMonsters = Future {
+        println("Parse JSON file ...")
+
+        val jFile = Source.fromFile("../monsters.json")
+        val jString = try jFile.mkString finally jFile.close()
+        val Some(json) = JSON.parseFull(jString)
+
+        class CastExtractor[T] {
+            def unapply(a:Any): Option[T] = Some(a.asInstanceOf[T])
+        }
+        val exList    = new CastExtractor[List[Any]]
+        val exMap     = new CastExtractor[Map[String, Any]]
+        val exStr     = new CastExtractor[String]
+        val exStrList = new CastExtractor[List[String]]
+
+        for {
+            // 'json' is wrapped into a list to allow the 'for comprehension' expression
+            exList(monsters) <- List(json)
+            exMap(monster)   <- monsters
+            exStr(name)       = monster("name")
+            exStrList(spells) = monster("spells")
+        } yield {
+            (name, spells)
+        }
+    }
+
+    val (sc, monsters) = Await.result(for {
+        _1 <- fSparkContext
+        _2 <- fMonsters
+    } yield (_1, _2), 5.second)
+
+    println("Manipulate data in RDDs ...")
+
+    val rddMonsters = sc.parallelize(monsters)
+    val rddSpells = rddMonsters
+        .flatMap(monster => {
+            for (spell <- monster._2)
+                yield (spell, monster._1)
+        })
+        .groupByKey()
+
+    val folder = new File("spells")
+    if (folder.exists) {
+        folder.listFiles.foreach(_.delete())
+        folder.delete()
+    }
+    rddSpells.saveAsTextFile(folder.getPath)
+
+    println("Spells have been written to 'ScalaRDD/spells'")
 }
