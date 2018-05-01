ffc6ccbe8133513995700693e3edc9735859bfbc
Sidney Congard <sidney.congard@gmail.com>
2018-05-01 13:05:28 -0400

    simplified SceneSetup

--- Devoir 2/Exercice 2/DDFights/src/UI/SceneSetup.scala
+++ Devoir 2/Exercice 2/DDFights/src/UI/SceneSetup.scala
@@ -4,16 +4,9 @@ import common.ActorSetup
 
 @SerialVersionUID(100L)
 class SceneSetup(
-                        _actorList: List[ActorSetup],
-                        _top: Double,
-                        _left: Double,
-                        _bottom: Double,
-                        _right: Double
-                ) extends Serializable{
-    var actorList: List[ActorSetup] = _actorList
-    var top: Double = _top
-    var left: Double = _left
-    var bottom: Double = _bottom
-    var right: Double = _right
-
-}
+        val actorList: List[ActorSetup],
+        val top: Double,
+        val left: Double,
+        val bottom: Double,
+        val right: Double) extends Serializable
+{}
